b705b6cb13621dde6645c5dedfe05bdf1dc37f8b
lingjie <chenjun2098@126.com>
2024-03-11 10:08:22 +0800

    11. Material 主题设置（https://developer.android.google.cn/codelabs/jetpack-compose-migration?hl=zh-cn#10）

--- MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
+++ MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
@@ -36,6 +36,7 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
 import com.google.samples.apps.sunflower.databinding.FragmentPlantDetailBinding
+import com.google.samples.apps.sunflower.theme.SunflowerTheme
 import com.google.samples.apps.sunflower.utilities.InjectorUtils
 import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
 
@@ -121,7 +122,7 @@ class PlantDetailFragment : Fragment() {
                     ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
                 )
                 setContent {
-                    MaterialTheme {
+                    SunflowerTheme {
                         PlantDetailDescription(plantDetailViewModel)
                     }
                 }
